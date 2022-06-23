319afd7858fd0f0df6086240d016a870b6eab1cb
Marcoshlp18 <marcoshenriqueball12@gmail.com>
2022-06-23 08:06:23 -0300

    Atividade Array VNW

---
+++ script.js
@@ -0,0 +1,20 @@
+// Crie um array que receba 5 itens e exiba no console:
+let frutas = ["Maça","Uva","Mamão","Abacaxi","Melão"];
+console.log("As frutas são:",frutas);
+// Utilize um método para adicionar um nome ao inicio do array:
+frutas.unshift("Abacate","Kiwi");
+console.log("Unsshift + Array frutas", frutas);
+// Utilize um método para remover o último nome do array:
+frutas.pop();
+console.log("Array com pop, removeu o Melão ",frutas)
+// Utilize um método para adicionar dois nomes ao fim do array:
+frutas.push("Bergamota","Banana");
+console.log("Array com push", frutas)
+// Utilize um método para remover o primeiro nome do array:
+frutas.shift();
+console.log("Arrey com shift", frutas)
+// Utilize um método para organizar em ordem crescente o seguinte array:
+
+let numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort();
+console.log(numbers)
